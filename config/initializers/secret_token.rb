1075dd921612f22945fd48b826a2921dbacdd5b9
Ezra Okon <Ezra.Okon@gmail.com>
2013-10-26 00:34:06 -0400

    Created admissions application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Admissions::Application.config.secret_token = 'd17c74656c7654cb21e428611fae0f99adc6b5d38e437c6e128d424d7b93c10346c2bfc4e942a1f38054576243b9ba270508ec0b3772d262420328810b727dd1'
